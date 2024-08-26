22f2757974ee10136f15b456bc6a91cbb65219fb
rohit-sonawane7 <rohitsonawane735@gmail.com>
2024-08-26 15:17:28 +0530

    log added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    console.log("checking logs not allowed rule");    
     res.send('Hello, World!');
 });
 
